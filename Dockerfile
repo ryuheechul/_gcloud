da89c05e1e0ab345b7a37651b0208deba4b1779c
Heechul Ryu <ryuhcii@gmail.com>
2017-05-08 10:31:02 +0900

    install vim. add volume extra/

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM google/cloud-sdk
 
+RUN apt-get update && apt-get install -y vim
+
 VOLUME ["/.kube"]
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -ti -p 8080:8001 --volumes-from gcloud-config gcloud-sdk-kube $@
+docker run --rm -ti -p 8080:8001 --volumes-from gcloud-config -v `pwd`/extra:/extra gcloud-sdk-kube $@
